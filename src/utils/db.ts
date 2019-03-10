9b13d38942223970e56227b921d1e8d9130784f7
Beppe Karlsson <beppe@live.se>
2019-03-10 12:51:57 +1100

    Add dbName param to db connection for tests

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,13 +2,13 @@ import * as mongoose from 'mongoose';
 
 const { NODE_ENV } = process.env;
 
-const dbName = NODE_ENV === 'test' ? 'test-csv-import' : 'csv-import';
+const DB_NAME = NODE_ENV === 'test' ? 'test-csv-import' : 'csv-import';
 
-const URI = `mongodb://localhost:27017/${dbName}`;
+const URI = `mongodb://localhost:27017`;
 
-export const connect = async (): Promise<void> => {
+export const connect = async (dbName = DB_NAME): Promise<void> => {
   try {
-    await mongoose.connect(URI, {
+    await mongoose.connect(`${URI}/${dbName}`, {
       useNewUrlParser: true,
       useCreateIndex: true,
     });
