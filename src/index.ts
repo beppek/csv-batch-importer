3723156207d34e7009db1cbe59ecaebfbeb91fe8
Beppe Karlsson <beppe@live.se>
2019-03-10 12:53:09 +1100

    Validate path and add path prompt

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,18 @@ import { performance } from 'perf_hooks';
 import chalk from 'chalk';
 import * as figlet from 'figlet';
 import * as inquirer from 'inquirer';
+import { PathPrompt } from 'inquirer-path';
 
-import { startImport } from './utils/importer';
+import { startImport, validatePath } from './utils/importer';
 import db from './utils/db';
+
+const printHeader = () => {
+  console.clear();
+  const font = 'Speed';
+  console.log(chalk.green(figlet.textSync('CSV BATCH', { font })));
+  console.log(chalk.magenta(figlet.textSync('IMPORTER', { font })));
+  console.log(chalk.blue('By Max Karlsson - https://github.com/beppek'));
+};
 
 export const init = async (path: string) => {
   try {
@@ -12,9 +21,7 @@ export const init = async (path: string) => {
     await db.connect();
     console.log(chalk.green('Connected.'));
     console.log(
-      chalk.yellow(
-        `Starting import from file at ./csv/${path}. Please wait ...`,
-      ),
+      chalk.yellow(`Starting import from file at ${path}. Please wait ...`),
     );
     const startTime = performance.now();
     const totalOrders = await startImport(path);
@@ -36,20 +43,30 @@ export const init = async (path: string) => {
   process.exit(0);
 };
 
+const getFilePath = async (errorMessage?: string) => {
+  try {
+    if (errorMessage) {
+      printHeader();
+      console.log(chalk.red(errorMessage));
+    }
+    inquirer.registerPrompt('path', PathPrompt);
+    const { path } = await inquirer.prompt([
+      {
+        name: 'path',
+        type: 'path',
+        message: 'Please, specify the path to the CSV file you wish to import',
+      },
+    ]);
+    validatePath(path);
+    await init(path);
+  } catch (error) {
+    await getFilePath(error.message);
+  }
+};
+
 const run = async () => {
-  console.clear();
-  const font = 'Speed';
-  console.log(chalk.green(figlet.textSync('CSV BATCH', { font })));
-  console.log(chalk.magenta(figlet.textSync('IMPORTER', { font })));
-  console.log(chalk.blue('By Max Karlsson - https://github.com/beppek'));
-  const { path } = await inquirer.prompt([
-    {
-      name: 'path',
-      type: 'input',
-      message: 'Please, specify the path to the CSV file you wish to import',
-    },
-  ]);
-  await init(path);
+  printHeader();
+  await getFilePath();
 };
 
 run();
